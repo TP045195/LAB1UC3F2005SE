14694e86ae68036b830030584f97d653f04c8d29
TP045195 <72290505+TP045195@users.noreply.github.com>
2020-10-09 10:28:27 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -19,6 +19,19 @@
           <th>Menara Standard Chartered, TPM</th>
           <th>03-22441234</th>
         </tr>
+        <?php
+          $connectionInfo = array("UID" => "apuadmin", "pwd" => "w0ndak0piTARIK", "Database" => "apulab2exercise", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+          $serverName = "tcp:lab2exercisetp045195.database.windows.net,1433";
+          $conn = sqlsrv_connect($serverName, $connectionInfo);
+        
+          //Establishes the connection
+          $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+          if (!$conn)
+          {
+           die("Error connection: ".sqlsrv_errors());
+          }
+        ?>
       </table>
     </center>
   </body>
